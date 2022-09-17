d42d98a7231ebcb6429f13f0ff479dd9cdcf2a48
Agunivers7 <110654906+Agunivers7@users.noreply.github.com>
2022-09-17 14:48:31 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'LuciferMoringstar_Robot'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:LuciferMoringstar_Robot-zyx57W2v1u123ew11'
+API_ID = 10651048
+API_HASH = '37775aca7d11f450ecde375baac17fe7'
+BOT_TOKEN = '5569567626:AAG4fEKnv998AZcoNjdt0WTAPLW56r8UdHc'
 USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
 
 # Bot settings
@@ -12,12 +12,12 @@ USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
 ADMINS = [12345789, 'admin123', 987654321]
-CHANNELS = [-10012345678, -100987654321, 'Mo_Tech_YT']
-AUTH_USERS = []
+CHANNELS = [1323557247]
+AUTH_USERS = [1323557247]
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[LuciferMoringstar_Robot:LuciferMoringstar_Robot@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://Aadhi:42426840@cluster0.h9rky.mongodb.net/?retryWrites=true&w=majority"
 DATABASE_NAME = 'LuciferMoringstar_Robot'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
