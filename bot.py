afcfbd1e4b3ecd930626cd507f82ce2965537748
Agunivers7 <herokuone7@gmail.com>
2022-10-22 19:16:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ class Bot(Client):
     def __init__(self):
         super().__init__(
             session_name=SESSION,
-            api_id=API_ID,
+            api_id=10651048,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             workers=50,
